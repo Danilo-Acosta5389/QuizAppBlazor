555f3406e807803036198dfbfac5b2ca72d1b144
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-11-23 21:46:14 +0100

    NewQuiz form, navmenu and new controller

---
+++ QuizAppBlazor/Server/Controllers/CreateQuizController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuizAppBlazor.Shared;
+
+namespace QuizAppBlazor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreateQuizController : ControllerBase
+    {
+    }
+}
