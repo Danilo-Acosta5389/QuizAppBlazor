3b110ee97aa8cb1fbc2f47740087ded81631992b
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-11-30 06:34:41 +0100

    Timer and data seed

--- QuizAppBlazor/Server/Data/ApplicationDbContext.cs
+++ QuizAppBlazor/Server/Data/ApplicationDbContext.cs
@@ -22,38 +22,96 @@ namespace QuizAppBlazor.Server.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            //modelBuilder.Entity<UserQuizModel>()
-            //    .HasKey(k => new { k.UserId, k.QuizId });
-
-            //modelBuilder.Entity<QuestionModel>()
-            //    .HasOne(q => q.Quiz).WithMany().OnDelete(DeleteBehavior.NoAction);
-
-            //modelBuilder.Entity<QuizModel>()
-            //    .HasOne<ApplicationUser>(u => u.User).WithMany().OnDelete(DeleteBehavior.NoAction);
 
             //Database seeding
-            //modelBuilder.Entity<QuizModel>().HasData(
-            //    new QuizModel
-            //    {
-            //        Id = Guid.NewGuid(),
-            //        Title = "Politics",
-            //        Description = "Test your knowledge about world politics.",
-            //        UserId = "05307715-cae7-45ca-b5d8-d30ec1705e2a"
-            //    },
-            //    new QuizModel
-            //    {
-            //        Id = Guid.NewGuid(),
-            //        Title = "Geography",
-            //        Description = "Test your knowledge about countrys and citys.",
-            //        UserId = "05307715-cae7-45ca-b5d8-d30ec1705e2a"
-            //    },
-            //    new QuizModel
-            //    {
-            //        Id = Guid.NewGuid(),
-            //        Title = "History",
-            //        Description = "Test your knowledge about world history.",
-            //        UserId = "05307715-cae7-45ca-b5d8-d30ec1705e2a"
-            //    });
+            modelBuilder.Entity<QuizModel>().HasData(
+                new QuizModel
+                {
+                    Id = Guid.Parse("F770E323-5463-49A9-97F1-0F88F6857D21"),
+                    LinkId = "masters-of-the-game-f770e323",
+                    Title = "\"Masters of the Game\"",
+                    Description = "Politics Quiz: Discover the intricate dance of power and governance in 'Masters of the Game.' Explore political systems, international relations, and economic ideologies that shape the world of politics.",
+                    UserId = "b429a22a-7746-416d-81ec-7837b1e28379"
+                });
+
+            modelBuilder.Entity<QuestionModel>().HasData(
+                new QuestionModel
+                {
+                    Id = 1,
+                    Question = "What is the term for a political system in which power is vested in the hands of a few individuals or a single entity?",
+                    CorrectAnswer = "Oligarchy",
+                    Alternativ2 = "Democracy",
+                    Alternativ3 = "Anarchy",
+                    Alternativ4 = "Communism",
+                    IsTextInput = false,
+                    ImageVideo = "https://s3.amazonaws.com/blog.oxfamamerica.org/politicsofpoverty/2015/04/Inequality-014-1220x763.jpg",
+                    IsImage = true,
+                    HasTimeLimit = true,
+                    TimeLimit = 20,
+                    QuizId = Guid.Parse("F770E323-5463-49A9-97F1-0F88F6857D21"),
+                    LinkId = "masters-of-the-game-f770e323"
+                },
+                new QuestionModel
+                {
+                    Id = 2,
+                    Question = "Which branch of the United States government is responsible for interpreting laws and ensuring they align with the Constitution?",
+                    CorrectAnswer = "Judicial",
+                    Alternativ2 = "Legislative",
+                    Alternativ3 = "Executive",
+                    Alternativ4 = "Administrative",
+                    IsTextInput = false,
+                    ImageVideo = "https://cdn.britannica.com/34/203634-049-D35DACFA.jpg",
+                    IsImage = true,
+                    HasTimeLimit = true,
+                    TimeLimit = 20,
+                    QuizId = Guid.Parse("F770E323-5463-49A9-97F1-0F88F6857D21"),
+                    LinkId = "masters-of-the-game-f770e323"
+                },
+                new QuestionModel
+                {
+                    Id = 3,
+                    Question = "In international relations, what is the concept that suggests countries should avoid interference in the internal affairs of other sovereign nations?",
+                    CorrectAnswer = "Isolationism",
+                    Alternativ2 = "Imperialism",
+                    Alternativ3 = "Globalism",
+                    Alternativ4 = "Interventionism",
+                    IsTextInput = false,
+                    ImageVideo = "y32cFdicW1U",
+                    IsYoutubeVideo = true,
+                    HasTimeLimit = true,
+                    TimeLimit = 20,
+                    QuizId = Guid.Parse("F770E323-5463-49A9-97F1-0F88F6857D21"),
+                    LinkId = "masters-of-the-game-f770e323"
+                },
+                new QuestionModel
+                {
+                    Id = 4,
+                    Question = "What is the primary function of the United Nations Security Council?",
+                    CorrectAnswer = "Conflict resolution",
+                    Alternativ2 = "Humanitarian aid",
+                    Alternativ3 = "Economic development",
+                    Alternativ4 = "Cultural exchange",
+                    IsTextInput = false,
+                    ImageVideo = "https://vod-progressive.akamaized.net/exp=1701334814~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2836%2F12%2F314181352%2F1212112677.mp4~hmac=f85c8a3f2832b0957da2af728fd1605894cd96de2b35504fdea2b43ae2b94e23/vimeo-prod-skyfire-std-us/01/2836/12/314181352/1212112677.mp4",
+                    IsVideo = true,
+                    HasTimeLimit = true,
+                    TimeLimit = 30,
+                    QuizId = Guid.Parse("F770E323-5463-49A9-97F1-0F88F6857D21"),
+                    LinkId = "masters-of-the-game-f770e323"
+                },
+                new QuestionModel
+                {
+                    Id = 5,
+                    Question = "Which economic system is characterized by private ownership of the means of production and individual entrepreneurship?",
+                    CorrectAnswer = "Capitalism",
+                    IsTextInput = true,
+                    ImageVideo = "https://bestdiplomats.org/wp-content/uploads/2023/10/Types-of-Political-Economy-copy-1.jpg",
+                    IsImage = true,
+                    HasTimeLimit = true,
+                    TimeLimit = 60,
+                    QuizId = Guid.Parse("F770E323-5463-49A9-97F1-0F88F6857D21"),
+                    LinkId = "masters-of-the-game-f770e323"
+                });
         }
     }
 }
