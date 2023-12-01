7c7b68ba4eef807a2171e86e9e22086a34397a00
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-12-01 21:29:31 +0100

    refatct

--- QuizAppBlazor/Shared/DTOs/GetQuestionsDTO.cs
+++ QuizAppBlazor/Shared/DTOs/GetQuestionsDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,8 +20,6 @@ namespace QuizAppBlazor.Shared.DTOs
         public string? Alternativ3 { get; set; }
 
         public string? Alternativ4 { get; set; }
-
-        public string? UserTextInput { get; set; }  //THIS FIELD IS NOT NEEDED
 
         public bool? IsTextInput { get; set; }
 
