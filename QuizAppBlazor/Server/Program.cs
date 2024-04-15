609fb5ea89f5794f74bb5b294c84a2debd217ece
Danilo Acosta <danilo.acostadote@chasacademy.se>
2024-04-15 21:33:40 +0200

    comment out line

--- QuizAppBlazor/Server/Program.cs
+++ QuizAppBlazor/Server/Program.cs
@@ -103,7 +103,7 @@ namespace QuizAppBlazor
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseCertificateForwarding();
+            //app.UseCertificateForwarding();
             app.UseHttpsRedirection();
 
             app.UseBlazorFrameworkFiles();
