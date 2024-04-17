f817259f452aaa1c33b4542413e16dee212a5bf9
Danilo Acosta <danilo.acostadote@chasacademy.se>
2024-04-17 13:20:24 +0200

    Cookie conf

--- QuizAppBlazor/Client/Program.cs
+++ QuizAppBlazor/Client/Program.cs
@@ -1,4 +1,4 @@
-//using Blazored.LocalStorage;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -12,7 +12,7 @@ namespace QuizAppBlazor.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
-            //builder.Services.AddBlazoredLocalStorageAsSingleton();
+            builder.Services.AddBlazoredLocalStorageAsSingleton();
             builder.Services.AddHttpClient("QuizAppBlazor.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
@@ -20,7 +20,7 @@ namespace QuizAppBlazor.Client
             builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("QuizAppBlazor.ServerAPI"));
 
             builder.Services.AddApiAuthorization();
-            //builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddBlazoredLocalStorage();
             await builder.Build().RunAsync();
         }
     }
--- QuizAppBlazor/Server/Program.cs
+++ QuizAppBlazor/Server/Program.cs
@@ -30,23 +30,41 @@ namespace QuizAppBlazor
             })
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
-            //builder.Services.AddCors(options =>
-            //{
-            //    options.AddPolicy(name: "BlazorCors",
-            //        policy =>
-            //        {
-            //            //policy.WithOrigins("https://localhost:7021")
-            //            policy.AllowAnyOrigin()
-            //            .AllowAnyHeader()
-            //            .AllowAnyMethod();
-            //        });
-            //});
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: "BlazorCors",
+                    policy =>
+                    {
+                        //policy.WithOrigins("https://localhost:7021")
+                        policy.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                    });
+            });
 
             builder.Services.AddIdentityServer()
                 .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
 
             builder.Services.AddAuthentication()
                 .AddIdentityServerJwt();
+
+            // Add services to the container.
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                options.ExpireTimeSpan = TimeSpan.FromDays(1);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.SameSite = SameSiteMode.None;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            });
+
+            //Configure external authentication cookies
+            builder.Services.ConfigureExternalCookie(options =>
+            {
+                options.ExpireTimeSpan = TimeSpan.FromDays(1);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.SameSite = SameSiteMode.None;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            });
 
             builder.Services.AddControllersWithViews();
             builder.Services.AddRazorPages();
@@ -74,14 +92,20 @@ namespace QuizAppBlazor
             app.UseStaticFiles();
 
             app.UseRouting();
-            //app.UseCors(builder => builder
-            //        .AllowAnyHeader()
-            //        .AllowAnyMethod()
-            //        .AllowAnyOrigin()
-            //    );
+            app.UseCors(builder => builder
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin()
+                );
+
             app.UseIdentityServer();
             app.UseAuthorization();
-
+            app.UseCookiePolicy(
+                new CookiePolicyOptions
+                {
+                    MinimumSameSitePolicy = SameSiteMode.None,
+                    Secure = CookieSecurePolicy.Always
+                });
 
             app.MapRazorPages();
             app.MapControllers();
